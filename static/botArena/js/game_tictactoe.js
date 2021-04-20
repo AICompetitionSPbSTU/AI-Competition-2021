74e2b64bf90d925e460c499abea2d6b6dbe5525b
Elizaveta Grishina <48994521+besperspektivnyak@users.noreply.github.com>
2021-04-20 17:42:25 +0300

    COD-67 tic-tac-toe game

--- static/botArena/js/game_tictactoe.js
+++ static/botArena/js/game_tictactoe.js
@@ -2,6 +2,9 @@ let origBoard;
 let botMove = false;
 const user = "O";
 const bot = "X";
+const button = document.querySelector(".nextStep");
+let state = Array(9).fill(-1);
+let occupied = Array(9).fill(false);
 
 
 const cells = document.querySelectorAll(".cell");
@@ -9,13 +12,14 @@ startGame();
 
 function StartRequestGet(){
     const request = new XMLHttpRequest();
-    const url = "/botArena/game/lisa_matches/play?game_cond=start";
+    const url = "/botArena/game/tic_tac_toe/play?game_cond=start";
     request.open('GET', url);
     request.setRequestHeader('Content-Type', 'application/x-www-form-url');
     request.addEventListener("readystatechange", () => {
         if (request.readyState === 4 && request.status === 200) {
             const response = JSON.parse(request.responseText);
             state = response.inner_state;
+            console.log(request.responseText);
         }
     });
     request.send('start');
@@ -24,15 +28,17 @@ function StartRequestGet(){
 
 function RequestRunning(inner_state){
     let request = new XMLHttpRequest();
-    const url = "/botArena/game/lisa_matches/play?game_cond=running&inner_state=" + inner_state;
+        console.log(request.readyState === 4, request.status === 200)
+
+    const url = "/botArena/game/tic_tac_toe/play?game_cond=running&inner_state=" + inner_state;
     request.open('GET', url);
     request.setRequestHeader('Content-Type', 'application/x-www-form-url');
     request.addEventListener("readystatechange", () => {
-
         if(request.readyState === 4 && request.status === 200) {
+            console.log('b event ')
             const response = JSON.parse(request.responseText);
-            state = response.inner_state;
-            console.log(request.responseText)
+	        state = response.inner_state;
+	        console.log(request.responseText)
         }
     });
 
@@ -40,22 +46,54 @@ function RequestRunning(inner_state){
 }
 
 function startGame() {
-    document.querySelector(".endgame").style.display = "none";
+    //document.querySelector(".endgame").style.display = "none";
     origBoard = Array.from(Array(9).keys());
     for (let i = 0; i < cells.length; i++) {
         cells[i].innerText = "";
         cells[i].style.removeProperty("background");
         cells[i].addEventListener("click", turnClick);
     }
+    button.addEventListener('click', ButtonEvent)
     StartRequestGet()
+    console.log('start')
 }
 
 function turnClick() {
     if(botMove === false) {
         document.getElementById(this.id).innerText = user;
+        state[this.id] = 0
+        occupied[this.id] = true;
+        console.log('user move' + state)
     }
     else {
         document.getElementById(this.id).innerText = bot;
+        occupied[this.id] = true;
+        botMove = false;
+        console.log('bot move')
     }
+}
 
+function Sleep(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+function ButtonEvent(){
+    console.log('button event ' + state)
+    RequestRunning(state);
+    Sleep(10).then(() => {
+        ImagineClick();
+    });
+}
+
+
+function ImagineClick(){
+    botMove = true;
+    for (let i = 0; i < state.length; i++) {
+        if (state[i] === 1 && occupied[i] === false){
+            //occupied = true;
+            cells[i].click();
+            return;
+        }
+    }
+    botMove = false;
 }
