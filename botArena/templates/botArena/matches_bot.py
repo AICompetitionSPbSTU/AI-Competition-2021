382279626e7887954b288fd5cb15ca4714f193f8
Dmitroved <dmitroved@gmail.com>
2021-05-05 19:28:50 +0300

    COD feature(view) add bot examples

---
+++ botArena/templates/botArena/matches_bot.py
@@ -0,0 +1,6 @@
+# Example bot for matches game
+
+
+class Bot:
+    def get_matches_number(self, current_number):
+        return 2
---
+++ botArena/templates/botArena/tic_tac_toe_bot.py
@@ -0,0 +1,8 @@
+# Example bot for tic_tac_toe game
+
+class Bot:
+    def get_step(self, current_field):
+        while True:
+            bot_choose = randint(0, 8)
+            if current_field[bot_choose] == -1:
+                return bot_choose
