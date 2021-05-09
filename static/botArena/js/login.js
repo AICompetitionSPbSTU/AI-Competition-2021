4d599ea3169b66e4c58fca9bbab698ed4d2803df
Anastasia <60839846+Brightest-Sunshine@users.noreply.github.com>
2021-05-09 20:54:14 +0300

    Nastya's work

--- static/botArena/js/login.js
+++ static/botArena/js/login.js
@@ -20,7 +20,8 @@ jQuery(function($) {
     var el_l = $("#login");
     if ( el_l.val().length < 4 ) {
       var v_login = true;
-      el_l.after('<span class="text-error for-login">Логин должен быть больше 3 символов</span>');
+      el_l.after('<span class="text-error for-login">\n' +
+          'Login must be more than 3 characters</span>');
       document.getElementById('login').style.color = "red";
       $(".for-login").css({top: el_l.position().top + el_l.outerHeight() + 2});
     }
@@ -39,7 +40,8 @@ jQuery(function($) {
     if ( el_p1.val() != el_p2.val() ) {
       var v_pass1 = true;
       var v_pass2 = true;
-      el_p1.after('<span class="text-error for-pass1">Пароли не совпадают!</span>');
+      el_p1.after('<span class="text-error for-pass1">\n' +
+          'Password mismatch!</span>');
       document.getElementById('pass1').style.color = "red";
         document.getElementById('pass2').style.color = "red";
       $(".for-pass1").css({top: el_p1.position().top + el_p1.outerHeight() + 2});
